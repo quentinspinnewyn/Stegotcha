bc74f06c3993193d5ddb08cfab4db091c6579a2b
GoldenDracoa <cailliez.alexandre@hotmail.fr>
2016-03-16 10:43:05 +0100

    Update Stegano
    Add length test

--- java/src/Steganographie.java
+++ java/src/Steganographie.java
@@ -11,7 +11,7 @@ import java.io.IOException;
  */
 public class Steganographie {
 
-    String os;  
+    String os;
     String p;
     Runtime runtime;
 
@@ -28,6 +28,7 @@ public class Steganographie {
     protected void insert(String embedTxt, String cover, String passphrase) {
         System.out.println("Starting Insertion");
         File tmp = new File ("resources"+p+"embedMsg.txt");
+        File coverFile = new File ("resources"+p+cover);
         try {
             FileWriter fw = new FileWriter(tmp);
             fw.write(embedTxt);
@@ -35,31 +36,37 @@ public class Steganographie {
         } catch (IOException e) {
             System.out.println("Erreur lors de l'écriture du message : "+e.getMessage());
         }
-        String[] cmd = new String[]{"","embed", "-ef resources"+p+"embedMsg.txt", "-cf "+"resources"+p+"cover","-p "+passphrase};
+        String[] cmd;
+        if (tmp.length()<cover.length()) {
+            System.out.println("Embed text has a correct length.");
+            cmd = new String[]{"", "embed", "-ef resources" + p + "embedMsg.txt", "-cf " + "resources" + p + "cover", "-p " + passphrase, "-z 9"};
 
-        /** TODO:
-         *  Vérifier si taille embed > taille cover
-         *  si oui : augmenter compression
-         *  Traitement message d'erreur !
-         */
+            /** TODO:
+             *  Vérifier si taille embed > taille cover
+             *  si oui : augmenter compression
+             *  Traitement message d'erreur !
+             */
 
-        if (os.contains("Windows")) {
-            System.out.println("Windows OS detected");
-            cmd[0]="resources"+p+"steghide"+p+"steghide.exe";
+            if (os.contains("Windows")) {
+                System.out.println("Windows OS detected");
+                cmd[0] = "resources" + p + "steghide" + p + "steghide.exe";
+            } else {
+                System.out.println("Other OS detected");
+                cmd[0] = "steghide";
+            }
+
+            try {
+                Process process = runtime.exec(cmd);
+                process.waitFor();
+            } catch (InterruptedException e) {
+                System.out.println("Embed execution has been interrupted");
+                e.printStackTrace();
+            } catch (IOException e) {
+                System.out.println("Embed execution failed");
+                e.printStackTrace();
+            }
         } else {
-            System.out.println("Other OS detected");
-            cmd[0]="steghide";
-        }
-
-        try {
-            Process process = runtime.exec(cmd);
-            process.waitFor();
-        } catch (InterruptedException e) {
-            System.out.println("Embed execution has been interrupted");
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.out.println("Embed execution failed");
-            e.printStackTrace();
+            System.out.println("Secret Message too big !");
         }
         System.out.println("Insertion ended");
     }
@@ -103,6 +110,7 @@ public class Steganographie {
          *  Traitement message d'erreur !
          */
         System.out.println("Exctraction correclty ended");
+        tmp.delete();
         return result;
     }
 
